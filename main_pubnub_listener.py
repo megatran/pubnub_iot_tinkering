6f2e61ea122dde09765380b0f6b50e013328c9d1
Nhan Tran <nhantrantnt@gmail.com>
2017-02-02 10:22:10 +0000

    added concurrency, improved ui

--- main_pubnub_listener.py
+++ main_pubnub_listener.py
@@ -1,6 +1,8 @@
 from pubnub import Pubnub
+from multiprocessing import Process
 import RPi.GPIO as GPIO
 import sys
+import time
 
 
 # setup pubnub
@@ -10,6 +12,7 @@ pubnub = Pubnub(
 	subscribe_key = "sub-c-c0bdecde-e682-11e6-a85c-0619f8945a4f")
 
 channel = "nhan_pi_tinker_channel"
+ultrasonic_channel = "nhan_ultrasonic_channel"
 
 
 def error(message):
@@ -31,51 +34,107 @@ def callback(message, channel):
 	print('[' + channel + ']: ' + "key pressed: " + str(message))
 	wiresless_control(message)
 
-pubnub.subscribe(channels=channel, callback=callback, error=callback,
-                 connect=connect, reconnect=reconnect, disconnect=disconnect)
+def ultrasonic_wiresless():
+
+	# ultrasonic sensor (HC-SR04) measures the distance to an object by using sound wave. Good choice for object avoidance.
+	# This publisher runs concurrently (using Python's multiprocessing) with the subscriber.
+	# All ultrasonic data (distance from robot to surrounding object) is published to the ultrasonic_channel.
+
+
+	GPIO.setmode(GPIO.BOARD)
+	# trig pin of the distance measurement sensor
+	TRIG = 36
+	# Define the echo pin of the distance measurement sensor
+	ECHO = 38
+
+	# set up ultrasonic sensor
+	GPIO.setwarnings(False)
+	GPIO.setup(TRIG,GPIO.OUT)                  
+	GPIO.setup(ECHO,GPIO.IN)
+
+	while True:    
+		pulse_start = 0
+		GPIO.output(TRIG, False)                
+		time.sleep(2)                            
+
+		GPIO.output(TRIG, True)                  
+		time.sleep(0.00001)                      
+		GPIO.output(TRIG, False)     
+
+		while GPIO.input(ECHO)==0:              
+		  pulse_start = time.time()            
+
+		while GPIO.input(ECHO)==1:               
+		  pulse_end = time.time()               
+
+		pulse_duration = pulse_end - pulse_start 
+		distance = pulse_duration * 17150
+		distance = int(distance)
+
+		print ("\nDistance of the object from Sensor = "+str(distance))
+		pubnub.publish(channel = ultrasonic_channel, message = distance)
 
 def wiresless_control(message):
-		# define GPIO pin
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(7,GPIO.OUT)
-		GPIO.setup(11,GPIO.OUT)
-		GPIO.setup(13,GPIO.OUT)
-		GPIO.setup(15,GPIO.OUT)
 
-		# only use this when send message via pubnub console. For example, it would retrieve {"text":"quit"}
-		#key_input = message[u'text'].replace("u\"","\"").replace("u\'","\'")
+	# this method subscribes to nhan_pi_tinker_channel which awaits for user's keyboard input from the sender to tell the motor what to do.
+	# this method and the ultrasonic_wiresless method both run at the same time!
+	
+	# define GPIO pin
+	GPIO.setmode(GPIO.BOARD)
 
-		# comment this line below if use pubnub console
-		key_input = message
-		
-		print key_input
-		if key_input == "quit":
-			print("QUIT")
-			GPIO.cleanup()
-			sys.exit(0)
+	GPIO.setup(7,GPIO.OUT)
+	GPIO.setup(11,GPIO.OUT)
+	GPIO.setup(13,GPIO.OUT)
+	GPIO.setup(15,GPIO.OUT)
 
-		elif key_input == "up":
-			GPIO.output(7,False)
-			GPIO.output(11,True)
-			GPIO.output(13,False)
-			GPIO.output(15,True)
-		elif key_input == "down":
-			GPIO.output(7,True)
-			GPIO.output(11,False)
-			GPIO.output(13,True)
-			GPIO.output(15,False)
-		elif key_input == "right":
-			GPIO.output(7,True)
-			GPIO.output(11,False)
-			GPIO.output(13,False)
-			GPIO.output(15,True)
-		elif key_input == "left":
-			GPIO.output(7,False)
-			GPIO.output(11,True)
-			GPIO.output(13,True)
-			GPIO.output(15,False)
-		elif key_input == "pause":
-			GPIO.output(7,False)
-			GPIO.output(11,False)
-			GPIO.output(13,False)
-			GPIO.output(15,False)
+
+	# only use this when send message via pubnub console. For example, it would retrieve {"text":"quit"}
+	#key_input = message[u'text'].replace("u\"","\"").replace("u\'","\'")
+
+	# comment this line below if use pubnub console
+	key_input = message
+	
+	print key_input
+	if key_input == "quit":
+		print("QUIT")
+		GPIO.cleanup()
+		sys.exit(0)
+
+	elif key_input == "up":
+		GPIO.output(7,False)
+		GPIO.output(11,True)
+		GPIO.output(13,False)
+		GPIO.output(15,True)
+	elif key_input == "down":
+		GPIO.output(7,True)
+		GPIO.output(11,False)
+		GPIO.output(13,True)
+		GPIO.output(15,False)
+	elif key_input == "right":
+		GPIO.output(7,True)
+		GPIO.output(11,False)
+		GPIO.output(13,False)
+		GPIO.output(15,True)
+	elif key_input == "left":
+		GPIO.output(7,False)
+		GPIO.output(11,True)
+		GPIO.output(13,True)
+		GPIO.output(15,False)
+	elif key_input == "pause":
+		GPIO.output(7,False)
+		GPIO.output(11,False)
+		GPIO.output(13,False)
+		GPIO.output(15,False)
+
+if __name__=='__main__':
+	# Having two process run concurrently:
+	# - the ultrasonic sensor publishes data to ultrasonic_channel
+	# - pubnub subscriber also waiting for command from remote user
+	# - when user pressed 'q', program will quit thus kill both processes.
+	p2 = Process(target= ultrasonic_wiresless)
+	# parent process will kill child process if q is pressed
+	p2.daemon = True
+	p2.start()
+
+pubnub.subscribe(channels=channel, callback=callback, error=callback,
+                 connect=connect, reconnect=reconnect, disconnect=disconnect)
--- main_pubnub_sender.py
+++ main_pubnub_sender.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from pubnub import Pubnub
 import curses
 
@@ -6,6 +7,7 @@ pubnub = Pubnub(
 	subscribe_key = "sub-c-c0bdecde-e682-11e6-a85c-0619f8945a4f")
 
 channel = "nhan_pi_tinker_channel"
+ultrasonic_channel = "nhan_ultrasonic_channel"
 message = "Test message"
 send_message = ""
 
@@ -15,23 +17,54 @@ curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
+screen.addstr(0,0, "Welcome to Pubnub Robot Remote Control -- created by Nhan Tran")
+screen.addstr(1,0, "Control your robot and get sensor input from anywhere in the world in real time!")
+
+screen.addstr(3,0, "First, make sure you RUN Pubnub listener 'main_pubnub_listener.py' on the raspberry pi")
+screen.addstr(4,0, "Press the Arrow keys: UP, DOWN, LEFT, RIGHT to control the motors in real time")
+screen.addstr(5,0, "Press 's' for Pause/Stop and 'q' to QUIT the program")
+screen.addstr(7,0, "=============================")
+
+screen.addstr(9,0, "Robot's movement direction: ")
+screen.addstr(11,0, "Distance of the object from Robot's Ultrasonic sensor (cm): ")
+
+
+def callback(message, channel):
+	var = message
+	screen.addstr(12,0, str(message)+" centimeters     ")
+	screen.refresh()
+
+pubnub.subscribe(channels=ultrasonic_channel, callback=callback)
+
 try:
 	while True:   
 		char = screen.getch()
 		if char == ord('q'):
+			screen.addstr(10,0, "EXITING PROGRAM...")
+			screen.refresh()
 			send_message = "quit"
 			# send last message before quitting
 			pubnub.publish(channel = channel, message = send_message)
 			break
 		elif char == curses.KEY_UP:
+			screen.addstr(10,0, "FORWARD         ")
+			screen.refresh()
 			send_message = "up"
 		elif char == curses.KEY_DOWN:
+			screen.addstr(10,0, "BACKWARD        ")
+			screen.refresh()
 			send_message = "down"
 		elif char == curses.KEY_RIGHT:
+			screen.addstr(10,0, "TURNING RIGHT         ")
+			screen.refresh()
 			send_message = "right"
 		elif char == curses.KEY_LEFT:
+			screen.addstr(10,0, "TURNING LEFT       ")
+			screen.refresh()
 			send_message = "left"
 		elif char == ord('s'):
+			screen.addstr(10,0, "PAUSED        ")
+			screen.refresh()
 			send_message = "pause"
 
 		pubnub.publish(
@@ -42,5 +75,4 @@ finally:
     # Close curses and turn echo back on
     curses.nocbreak(); screen.keypad(0); curses.echo()
     curses.endwin()
-    
     
